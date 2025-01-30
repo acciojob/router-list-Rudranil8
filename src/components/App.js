e2d94c3e4992590a86d1e33416bc0eb3a205ce55
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2025-01-30 21:15:40 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,45 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-import React from "react";
-import './../styles/App.css';
+const ListPage = () => {
+  return (
+    <div>
+      <h2>List Page</h2>
+      <ul>
+        <li>
+          <Link to="/items/1">Item 1</Link>
+        </li>
+        <li>
+          <Link to="/items/2">Item 2</Link>
+        </li>
+        <li>
+          <Link to="/items/3">Item 3</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+const ItemDetailsPage = ({ match }) => {
+  const itemId = match.params.itemId;
+
+  return (
+    <div>
+      <h2>Item Details Page</h2>
+      <p>Item ID: {itemId}</p>
+    </div>
+  );
+};
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <Router>
+      <div>
+        <Route exact path="/" component={ListPage} />
+        <Route path="/items/:itemId" component={ItemDetailsPage} />
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
